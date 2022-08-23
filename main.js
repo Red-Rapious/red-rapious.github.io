9fb776971f3bf18d2838f8d1db760dde63781c12
Red-Rapious <red.rapious@gmail.com>
2022-08-23 15:13:31 +0200

    Added some light

--- main.js
+++ main.js
@@ -17,14 +17,26 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 15, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+const material = new THREE.MeshStandardMaterial({ color: 0xcc0000	});
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+
+scene.add(pointLight, ambientLight);
+
 function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
 }
 
 animate();
